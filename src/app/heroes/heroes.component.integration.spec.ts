9c22dfa33c5f06a897eba5eb2c686c2d18ceb6da
Derek Wang <guanghuiw@vmware.com>
2019-07-04 18:52:50 +0800

    mock child component

--- src/app/heroes/heroes.component.integration.spec.ts
+++ src/app/heroes/heroes.component.integration.spec.ts
@@ -1,16 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { NO_ERRORS_SCHEMA, Input, Component } from '@angular/core';
 
 import { of } from 'rxjs';
 
 import { HeroService } from '../hero.service';
 
 import { HeroesComponent } from './heroes.component';
+import { Hero } from '../hero';
 
 describe('HeroesComponent (shallow tests)', () => {
   let fixture: ComponentFixture<HeroesComponent>;
   let mockHeroService;
   let HEROES;
+
+  // mock child component
+  @Component({
+    selector: 'app-hero',
+    template: '<div></div>',
+  })
+  class FakeHeroComponent {
+    @Input() hero: Hero;
+  }
 
   beforeEach(() => {
     HEROES = [
@@ -22,9 +32,9 @@ describe('HeroesComponent (shallow tests)', () => {
     mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero']);
 
     TestBed.configureTestingModule({
-      declarations: [HeroesComponent],
+      declarations: [HeroesComponent, FakeHeroComponent],
       providers: [{ provide: HeroService, useValue: mockHeroService }],
-      schemas: [NO_ERRORS_SCHEMA],
+      // schemas: [NO_ERRORS_SCHEMA],  use FakeHeroComponent, cuz NO_ERRORS_SCHEMA hide other problems
     });
     fixture = TestBed.createComponent(HeroesComponent);
   });
